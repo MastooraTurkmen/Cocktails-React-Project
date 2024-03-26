7d8c6440e0094ccdd2fd50840b5f7abdf4b0829e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-26 12:49:02 +0430

    set up loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 const Loading = () => {
- return (
-  <div className="loader">
-  </div>
- )
+    return (
+        <div className="loader"></div>
+    )
 }
 
-export default Loading
+export default Loading;
