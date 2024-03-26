56f6fde4a7371d145b8411dfea39f91ba229049f
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-26 12:48:31 +0430

    cocktail list

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,6 +4,20 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        no cocktails matched your search criteria
+      </h2>
+    )
+  }
+
   return (
     <div>
       <h2>cocktail list component</h2>
