aba5d96adc0c9fce415dad4c660e253a33113e40
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-26 12:49:35 +0430

    adds searchform and cocktailList

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,8 @@ import SearchForm from '../components/SearchForm'
 const Home = () => {
   return (
     <div>
-      <h2>home page</h2>
+      <SearchForm />
+      <CocktailList />
     </div>
   )
 }
