1b682050517cb7793865f86a37392cdc76f47c4e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-26 09:32:24 +0430

    set up app js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,26 @@ import SingleCocktail from './pages/SingleCocktail'
 import Error from './pages/Error'
 // import components
 import Navbar from './components/Navbar'
+
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <React>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/cocktail/:id'>
+          <SingleCocktail />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+    </React>
   )
 }
 
