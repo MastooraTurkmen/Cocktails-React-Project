70f919b0bf0ccea88e0d7cf69e453a9da4adf14e
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-26 14:25:53 +0430

    set up cocktails

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,7 +4,7 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
-  const { loading, cocktails } = useGlobalContext();
+  const { cocktails, loading } = useGlobalContext();
 
   if (loading) {
     return <Loading />
@@ -19,9 +19,16 @@ const CocktailList = () => {
   }
 
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className='section'>
+      <h2 className="section-title">
+        cocktails
+      </h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />
+        })}
+      </div>
+    </section>
   )
 }
 
