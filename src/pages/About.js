65313b8c9a789a6869de3b0d643e8b3cef85ecb2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-26 11:39:57 +0430

    set up about

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Magnam saepe, quisquam ea doloremque in unde ipsa deleniti quaerat ex sit!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, ex?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, aspernatur.
+      </p>
+    </section>
   )
 }
 
