94f4a724cca45fa2973b40b3fc18915f2d59a8ed
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-26 14:25:56 +0430

    set up cocktail

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Cocktail = () => {
+const Cocktail = ({ image, name, info, glass, id }) => {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
+    <section className='cocktails'>
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link to={'/cocktails/:id'} className='btn btn-primary'>
+          details
+        </Link>
+      </div>
+    </section>
   )
 }
 
