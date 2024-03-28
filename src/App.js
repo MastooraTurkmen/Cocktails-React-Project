17630110fec4d8c50185b0e91ccce594c14120af
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-28 13:16:16 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/cocktail/:id' element={<SingleCocktail />} />
         <Route path='*' element={<Error />} />
