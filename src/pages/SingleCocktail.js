b687c359c97d6c3ff210ca7ca7ff250ea2ab840d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-28 13:18:16 +0430

    first commit

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Loading from '../components/Loading'
 import { useParams, Link } from 'react-router-dom'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
+  const [loading, setLoading] = useState(false)
   return (
     <div>
       <h2>single cocktail page </h2>
