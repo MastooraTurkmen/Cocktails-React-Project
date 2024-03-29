3eae863f333aec6796bfff52291c53dca70ee3cb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-29 13:38:02 +0430

    set up single cocktail

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -16,7 +16,29 @@ const SingleCocktail = () => {
         const data = await response.json();
 
         if (data.drinks) {
-          console.log(data.drinks);
+          const {
+            idDrink: id,
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5
+          } = data.drinks[0]
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5]
+
+          const newCocktail = { name, id, info, category, glass, ingredients, instructions }
+          setCocktail(newCocktail)
         } else {
           setCocktail(null)
         }
