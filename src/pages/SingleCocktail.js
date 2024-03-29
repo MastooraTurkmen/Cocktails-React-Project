7eeb454cce7cb3afda863e2d38b4d136438efce0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-29 13:52:27 +0430

    set up single cocktail

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -36,7 +36,7 @@ const SingleCocktail = () => {
             strIngredient4,
             strIngredient5]
 
-          const newCocktail = { name, info, category, glass, ingredients, instructions }
+          const newCocktail = { name, image, info, category, glass, ingredients, instructions }
           setCocktail(newCocktail)
         } else {
           setCocktail(null)
@@ -59,12 +59,39 @@ const SingleCocktail = () => {
     return <h2 className="section-title">no cocktail to display</h2>
   }
 
-  const { name, info, category, glass, ingredients, instructions } = cocktail
+  const { name, image, info, category, glass, ingredients, instructions } = cocktail
 
   return (
-    <div>
-      <h2>single cocktail page </h2>
-    </div>
+    <section>
+      <Link to='/' className='btn btn-primary'>back home</Link>
+      <h2 className='section-title'>{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+        <div className="drink-info">
+          <p>
+            <span className='drink-data'>name: </span> {name}
+          </p>
+          <p>
+            <span className='drink-data'>category: </span> {category}
+          </p>
+          <p>
+            <span className='drink-data'>glass: </span> {glass}
+          </p>
+          <p>
+            <span className='drink-data'>instructions: </span> {instructions}
+          </p>
+          <p>
+            <span className='drink-data'>info: </span> {info}
+          </p>
+          <p>
+            <span className='drink-data'>ingredients: </span>
+            {ingredients.map((item, index) => {
+              return item ? <span key={index}>{item}</span> : null
+            })}
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
